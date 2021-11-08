d5f8d52adb19b69b490d58631963a9e19f026f23
Juan Carlos Valdes <idevkingos@gmail.com>
2021-11-08 18:59:58 -0300

    feat: Modelo y migracion de Post

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 
+
 Route::get('/', function () {
-    return view('welcome');
+    $posts = App\Models\Post::latest('published_at')->get();
+
+    // $posts = App\Models\Post::all();
+
+    // return view('welcome')->with('posts', $posts);
+    return view('welcome', compact('posts'));
+
+});
+
+Route::get('posts', function () {
+    return \App\Models\Post::all();
 });
